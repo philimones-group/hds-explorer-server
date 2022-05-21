bbce208a97dc87f85ce4994a53a9af6a42a28efa
Paulo Filimone <paulphilimone@gmail.com>
2022-05-22 00:30:50 +0200

    Removing liquibase domain ddl for rebase

--- grails-app/migrations/changelog.groovy
+++
@@ -1,6 +0,0 @@
-databaseChangeLog = {
-    include file: 'create-initial-tables.groovy'
-    include file: 'fix-raw_marital_relationship-constraints.groovy'
-    include file: 'add-parent_region_code_to_region.groovy'
-    include file: 'add-event_order-to-rawevent.groovy'
-}
--- grails-app/migrations/fix-raw_marital_relationship-constraints.groovy
+++
@@ -1,6 +0,0 @@
-databaseChangeLog = {
-
-    changeSet(author: "paul (generated)", id: "1652812844803-1") {
-        addNotNullConstraint(columnDataType: "varchar(255)", columnName: "collected_by", tableName: "_raw_marital_relationship", validate: "true")
-    }
-}
